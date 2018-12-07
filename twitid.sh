e5e2528fa96e9871b3498d3ed8a4165d34a3ecc1
Derp <felention@users.noreply.github.com>
2018-12-07 11:42:44 +0000

    Update twitid.sh

--- twitid.sh
+++ twitid.sh
@@ -5,21 +5,21 @@ if [[ -z $1 ]]; then
     echo "Enter a handle."
     exit 0
 fi
-sus=$(curl -I https://twitter.com/$1 --silent | grep -E "location: https://twitter.com/account/suspended")
-ex=$(curl -I https://twitter.com/$1 --silent | grep -E "status: 404 Not Found")
+sus=$(curl -I https://twitter.com/$1 --silent | grep -o "location: https://twitter.com/account/suspended")
+ex=$(curl -I https://twitter.com/$1 --silent | grep -o "status: 404 Not Found")
 if [[ -z $(ls $d | grep "TwitterIDs.txt") ]]; then
     touch $f
 fi
 if [[ $1 =~ ^[0-9]+$ ]]; then
-    ex=$(curl -I "https://twitter.com/intent/user?user_id=$1" --silent | grep -E "status: 404 Not Found")
-    sus=$(curl -I "https://twitter.com/intent/user?user_id=$1" --silent | grep -E "location: https://twitter.com/account/suspended")
+    ex=$(curl -I "https://twitter.com/intent/user?user_id=$1" --silent | grep -o "status: 404 Not Found")
+    sus=$(curl -I "https://twitter.com/intent/user?user_id=$1" --silent | grep -o "location: https://twitter.com/account/suspended")
     if [[ -n $ex ]]; then
-        echo "ID is invalid"
+        echo "ID is invalid."
         exit 0
     elif [[ -n $sus ]]; then
         echo "User is suspended"
         if [[ -n $(grep -iF "$1" $f) ]]; then
-            echo "Usernames were: $(grep "$1" $f)"
+            echo "Usernames were: $(grep -iF "$1" $f)"
         fi
         exit 0
     else
@@ -31,10 +31,13 @@ if [[ $1 =~ ^[0-9]+$ ]]; then
     fi
     exit 0
 elif [[ -n $sus ]]; then
-    echo "User is suspended"
+    echo "User is suspended."
+    if [[ -n $(grep -iF "$1" $f) ]]; then
+        echo "Usernames were: $(grep -iF "$1" $f)"
+    fi
     exit 0
 elif [[ -n $ex ]]; then
-    echo "User no longer exists"
+    echo "User no longer exists."
     if [[ -n $(grep -iF "$1" $f) ]]; then
         echo "ID was $(grep "$1" $f)"
     fi
